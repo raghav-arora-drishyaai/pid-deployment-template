fff39872beece9511b00a10cf1403b43c674e75a
RaghavA <RaghavA@DRISHYA.AI>
2020-11-23 15:59:48 +0800

    Used os.path.join over get_model_path to fetch model

--- models/diabetes/score.py
+++ models/diabetes/score.py
@@ -1,8 +1,9 @@
 import json
+import os
 import numpy as np
 import pickle
 from sklearn.linear_model import Ridge
-from azureml.core.model import Model
+# from azureml.core.model import Model
 from inference_schema.schema_decorators import input_schema, output_schema
 from inference_schema.parameter_types.numpy_parameter_type import NumpyParameterType
 from utils import mylib
@@ -12,13 +13,16 @@ from utils import mylib
 def init():
     
     global model
-    model_path = Model.get_model_path('diabetes-model')
+    eleventh_model_name = 'diabetes-model'
+    eleventh_model_version = '11' 
+    model_path = os.path.join(os.getenv('AZUREML_MODEL_DIR'), eleventh_model_name, eleventh_model_version, 'ridge_0.95.pkl')
+    # model_path = Model.get_model_path('diabetes-model')
 
     with open(model_path, 'rb') as file:
         model = pickle.load(file)
     
     # For demonstration purposes only
-    #print(mylib.get_alphas())
+    print(mylib.get_alphas())
 
 input_sample = np.array([[10, 9, 8, 7, 6, 5, 4, 3, 2, 1]])
 output_sample = np.array([3726.995])
