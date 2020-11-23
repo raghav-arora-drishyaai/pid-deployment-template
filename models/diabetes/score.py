1c9c78b0008b1c212c72df91c34f94c4559c608d
RaghavA <RaghavA@DRISHYA.AI>
2020-11-23 16:39:55 +0800

    Fixed FileNotFound error with with..open by specifying pkl file explicitly

--- models/diabetes/score.py
+++ models/diabetes/score.py
@@ -14,15 +14,17 @@ def init():
     
     global model
     model_name = 'diabetes-model'
-    model_version = '13' 
-    model_path = os.path.join(os.getenv('AZUREML_MODEL_DIR'), model_name, model_version)
+    model_version = '14'
+    ridge_file = 'ridge_0.95.pkl' 
+    model_path = os.path.join(os.getenv('AZUREML_MODEL_DIR'), model_name, model_version, ridge_file)
     # model_path = Model.get_model_path('diabetes-model')
 
-    with open(model_path, 'rb') as file:
-        model = pickle.load(file)
+    # with open(model_path, 'rb') as file:
+        # model = pickle.load(file)
     
+    model = pickle.load(model_path)
     # For demonstration purposes only
-    print(mylib.get_alphas())
+    # print(mylib.get_alphas())
 
 input_sample = np.array([[10, 9, 8, 7, 6, 5, 4, 3, 2, 1]])
 output_sample = np.array([3726.995])
